9fc7726abac056f19b8f8c3447f36883fba413c3
ovresko@github.com <ovresko@github.com>
2017-10-27 20:53:41 +0100

    none

--- elvaapp/alimentation/doctype/programme/programme.js
+++ elvaapp/alimentation/doctype/programme/programme.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2017, ovresko tech and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Programme', {
-	refresh: function(frm) {
-
+frappe.ui.form.on('aliments', {
+	onload: function(frm) {
+		cur_frm.set_query('aliment', function() {
+			return {
+				"filters": {
+					"item_group": "Consommable"
+				}
+			};
+		});
 	}
 });
